fe78ac3248c2a465cf010a1127f847bce721a319
axilirate <56825186+axilirate@users.noreply.github.com>
2021-06-26 22:47:43 +0300

    backup

--- src/main/java/com/elunar/bitsforbump/BitsForBump.java
+++ src/main/java/com/elunar/bitsforbump/BitsForBump.java
@@ -1,8 +1,11 @@
 package com.elunar.bitsforbump;
 
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BitsForBump extends JavaPlugin {
+public final class BitsForBump extends JavaPlugin implements Listener {
+
+    private DiscordSRVListener discordsrvListener = new DiscordSRVListener(this);
 
     @Override
     public void onEnable() {
---
+++ src/main/java/com/elunar/bitsforbump/DiscordSRVListener.java
@@ -0,0 +1,11 @@
+package com.elunar.bitsforbump;
+
+import org.bukkit.plugin.Plugin;
+
+public class DiscordSRVListener {
+    private final Plugin plugin;
+
+    public DiscordSRVListener(Plugin plugin) {
+        this.plugin = plugin;
+    }
+}
