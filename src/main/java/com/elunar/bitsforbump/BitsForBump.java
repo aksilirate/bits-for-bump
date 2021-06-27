1130dab1e2141073d4a4d5978bd2e8649874c8e2
axilirate <56825186+axilirate@users.noreply.github.com>
2021-06-27 18:20:38 +0300

    update

--- src/main/java/com/elunar/bitsforbump/BitsForBump.java
+++ src/main/java/com/elunar/bitsforbump/BitsForBump.java
@@ -1,20 +1,48 @@
 package com.elunar.bitsforbump;
 
-import org.bukkit.event.Listener;
+import github.scarsz.discordsrv.DiscordSRV;
+import net.milkbowl.vault.economy.Economy;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BitsForBump extends JavaPlugin implements Listener {
+
+public final class BitsForBump extends JavaPlugin {
 
     private DiscordSRVListener discordsrvListener = new DiscordSRVListener(this);
 
+    public static Economy eco;
+
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+
+
+        DiscordSRV.api.subscribe(discordsrvListener);
+
+        if (!setupEconomy() ) {
+            System.out.println("Disabled due to no Vault dependency found!");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }else{
+            RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+            if (rsp != null){
+                eco = rsp.getProvider();
+            }
+        }
+
+    }
+
+
+    private boolean setupEconomy() {
+        return getServer().getPluginManager().getPlugin( "Vault") != null;
 
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        DiscordSRV.api.unsubscribe(discordsrvListener);
     }
+
+
 }
--- src/main/java/com/elunar/bitsforbump/DiscordSRVListener.java
+++ src/main/java/com/elunar/bitsforbump/DiscordSRVListener.java
@@ -1,11 +1,47 @@
 package com.elunar.bitsforbump;
 
+import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.api.ListenerPriority;
+import github.scarsz.discordsrv.api.Subscribe;
+import github.scarsz.discordsrv.api.events.DiscordGuildMessageReceivedEvent;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
+import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
 import org.bukkit.plugin.Plugin;
 
+import java.util.UUID;
+
 public class DiscordSRVListener {
+
     private final Plugin plugin;
 
     public DiscordSRVListener(Plugin plugin) {
         this.plugin = plugin;
+
     }
+
+    @Subscribe(priority = ListenerPriority.MONITOR)
+    public void discordMessageReceived(DiscordGuildMessageReceivedEvent event) {
+        User user = event.getMember().getUser();
+        UUID uuid = DiscordSRV.getPlugin().getAccountLinkManager().getUuid(user.getId());
+
+        TextChannel textChannel = event.getChannel();
+
+
+        if (textChannel.getId().equals("852519391488901141")) {
+            if (event.getMessage().getContentRaw().equals("!d bump")) {
+                if (uuid != null) {
+                    String playerName = plugin.getServer().getOfflinePlayer(uuid).getName();
+                    textChannel.sendMessage("Thanks for attempting to bump! you have gained 1 bit.").queue();
+                    BitsForBump.eco.depositPlayer(playerName, 1.0);
+
+                } else {
+                    textChannel.sendMessage("account not linked.").queue();
+                }
+            }
+        }
+
+
+    }
+
+
 }
