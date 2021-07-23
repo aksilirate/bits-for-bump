bbcaff9f96fa7549ef22b7c7d4bc14e451af9dc1
axilirate <56825186+axilirate@users.noreply.github.com>
2021-07-23 11:36:53 +0300

    update

--- src/main/java/com/elunar/bitsforbump/DiscordSRVListener.java
+++ src/main/java/com/elunar/bitsforbump/DiscordSRVListener.java
@@ -6,6 +6,8 @@ import github.scarsz.discordsrv.api.Subscribe;
 import github.scarsz.discordsrv.api.events.DiscordGuildMessageReceivedEvent;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.TextChannel;
 import github.scarsz.discordsrv.dependencies.jda.api.entities.User;
+import org.bukkit.OfflinePlayer;
+
 import java.util.UUID;
 
 public class DiscordSRVListener {
@@ -31,12 +33,13 @@ public class DiscordSRVListener {
             if (event.getMessage().getContentRaw().equals("!d bump")) {
                 if (uuid != null) {
                     Integer playerBumps = bitsForBump.dataManager.getYamlPlayerBumps(user.getId());
-                    String playerName = bitsForBump.getServer().getOfflinePlayer(uuid).getName();
+                    OfflinePlayer offlinePlayer = bitsForBump.getServer().getOfflinePlayer(uuid);
                     textChannel.sendMessage("Thanks for attempting to bump, you have gained **1✦**.").queue();
-                    bitsForBump.eco.depositPlayer(playerName, 1.0);
+                    bitsForBump.eco.depositPlayer(offlinePlayer, 1.0);
                     bitsForBump.dataManager.setYamlPlayerBumps(user.getId(), playerBumps + 1);
                 } else {
-                    textChannel.sendMessage("account not linked.").queue();
+                    event.getChannel().sendMessage("**Your account is not linked.**").queue();
+                    event.getChannel().sendMessage("> Use the command `/discord link` in the minecraft server.").queue();
                 }
             }
         }
